f90a3642398bd98e6381e9f08549a1e0bbea32fb
Felipe Korhonen <127225031+FelipeKorhonen@users.noreply.github.com>
2023-05-16 13:55:14 -0300

    criei o javascript e adicionei imagens

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.backgronud = '#b9846f'
+    } else {
+        img.scr = 'imagens/noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}    
